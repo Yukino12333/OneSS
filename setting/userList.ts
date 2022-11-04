d7916346bbf8ce7ade1abecd24cd68d80ebad142
Yukino12333 <99794710+Yukino12333@users.noreply.github.com>
2022-11-04 13:15:11 +0800

    Update userList.ts

--- setting/userList.ts
+++ setting/userList.ts
@@ -1,8 +1,7 @@
 const userList: userListType = {
-    users: ["oss@2ld7nn.onmicrosoft.com", "ossd@2ld7nn.onmicrosoft.com"],
+    users: ["oss@2ld7nn.onmicrosoft.com"],
     nickname: {
-        "oss@2ld7nn.onmicrosoft.com": '🤝 分享功能测试',
-        "ossd@2ld7nn.onmicrosoft.com": '🌟 ☁OneSS † 开发测试用 †',
+        "oss@2ld7nn.onmicrosoft.com": '🤝 好事',
     }
 }
 export default userList;
